1e22ba0d9d248ad83928650acf7e76fc8e9397bc
Gowtham <gowthamushadevi@gmail.com>
2025-04-18 20:05:04 +0530

    updated

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,9 @@
-import unittest
-from test_hello import greet  
-
-class TestGreet(unittest.TestCase):
-    def test_greet_output(self):
-        self.assertEqual(greet(), "Hello, World!")
+from hello import greet
+def test_greet_output():
+        assert(greet())== "Hello, World!"
 
 if __name__ == "__main__":
-    unittest.main()
+    test_greet_output()
+    print("test greet output")
+    print("success")
+
